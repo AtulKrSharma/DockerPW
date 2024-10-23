9374f86a76dcc42d4fafa0ad86108dc84c78a1eb
Atul Sharma <49794348+AtulKrSharma@users.noreply.github.com>
2024-10-23 12:44:14 -0400

    Update File1.spec.ts

--- tests/File1.spec.ts
+++ tests/File1.spec.ts
@@ -52,3 +52,15 @@ test("File1 Test3", async ({ page }) => {
 
   await page.waitForTimeout(5000);
 });
+
+test("File1 Test4", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+});
